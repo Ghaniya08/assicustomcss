bb3a77e5784d86da49ed827f7c0486df8d883b8d
Ghaniya khan <165418041+Ghaniya08@users.noreply.github.com>
2024-11-10 19:43:18 +0500

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,7 @@ const Homee = () => {
       <div className='andr'>
         <Image src={image} alt='cho'/>
         <h1>WelCome to oue Website</h1>
-        <p>All you need is love. But a little chocolate now and then doesn't hurt." - Charles M. Schulz "What you see before you, my friend, is the result of a lifetime of chocolate." - Katharine Hepburn "As long as there is chocolate, there will be happiness.</p>
+        <p>All you need is love. But a little chocolate now and then doesnt hurt. - Charles M. Schulz What you see before you, my friend, is the result of a lifetime of chocolate. - Katharine Hepburn As long as there is chocolate, there will be happiness.</p>
         <button>Contact Us</button>
       </div>
     </div>
